4b96f0a1eab61787d3ce4701b9e52340fda053a3
Chuks Ricardo <C0628005@vale.com>
2025-05-27 03:30:42 -0300

    init

---
+++ order-management-ssm/src/main/java/br/com/chuksricardo/order_management_ssm/OrderManagementSsmApplication.java
@@ -0,0 +1,13 @@
+package br.com.chuksricardo.order_management_ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementSsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementSsmApplication.class, args);
+	}
+
+}
---
+++ order-management-ssm/src/test/java/br/com/chuksricardo/order_management_ssm/OrderManagementSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chuksricardo.order_management_ssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
