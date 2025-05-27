1437352c99fd8ffe4d96e3553488e0e46ed24b9b
Chuks Ricardo <C0628005@vale.com>
2025-05-27 03:51:07 -0300

    configuração de estados

--- order-management-ssm/src/main/java/br/com/chuksricardo/order_management_ssm/StateMachineConfig.java
+++ order-management-ssm/src/main/java/br/com/chuksricardo/order_management_ssm/StateMachineConfig.java
@@ -1,11 +1,22 @@
 package br.com.chuksricardo.order_management_ssm;
 
+import java.util.EnumSet;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 
 @Configuration
 @EnableStateMachineFactory
 public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<OrderState, OrderEvents>{
-  
+  @Override
+  public void configure(StateMachineStateConfigurer<OrderState, OrderEvents> states) throws Exception {
+    states
+    .withStates()
+    .initial(OrderState.NEW)
+    .states(EnumSet.allOf(OrderState.class))
+    .end(OrderState.COMPLETED)
+    .end(OrderState.CANCELED);
+  }
 }
