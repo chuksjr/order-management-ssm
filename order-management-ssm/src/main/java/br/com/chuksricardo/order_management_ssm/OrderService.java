dbdfcc2081b0b29e3959ae0f92c7a3aff7ae8799
Chuks Ricardo <C0628005@vale.com>
2025-05-27 04:47:30 -0300

    Configuração de Ordem de Serviço

---
+++ order-management-ssm/src/main/java/br/com/chuksricardo/order_management_ssm/OrderService.java
@@ -0,0 +1,71 @@
+package br.com.chuksricardo.order_management_ssm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class OrderService {
+  @Autowired
+  private StateMachineFactory<OrderState, OrderEvents> stateMachineFactory;
+  private StateMachine<OrderState, OrderEvents> stateMachine;
+
+  public void newOrder(){
+    initOrderSaga();
+    validadeOrder();
+    payOrder();
+    shipOrder();
+  }
+
+  private void validadeOrder(){
+    System.out.println("Validating order");
+    stateMachine.sendEvent(Mono.just(
+      MessageBuilder.withPayload(OrderEvents.VALIDATE).build()))
+      .subscribe(result -> System.out.println(result.getResultType()));
+      System.out.println("Final state: " + stateMachine.getState().getId());
+  }
+
+  private void payOrder(){
+    System.out.println("Paying order");
+    stateMachine.sendEvent(Mono.just(
+      MessageBuilder.withPayload(OrderEvents.PAY).build()))
+      .subscribe(result -> System.out.println(result.getResultType()));
+      System.out.println("Final state: " + stateMachine.getState().getId());
+  }
+
+  private void shipOrder(){
+    System.out.println("Shipping order");
+    stateMachine.sendEvent(Mono.just(
+      MessageBuilder.withPayload(OrderEvents.SHIP).build()))
+      .subscribe(result -> System.out.println(result.getResultType()));
+      System.out.println("Final state: " + stateMachine.getState().getId());
+  }
+
+  private void completeOrder(){
+    System.out.println("Completing order");
+    stateMachine.sendEvent(Mono.just(
+      MessageBuilder.withPayload(OrderEvents.COMPLETE).build()))
+      .subscribe(result -> System.out.println(result.getResultType()));
+      System.out.println("Final state: " + stateMachine.getState().getId());
+
+      stopOrderSaga();
+  }
+
+  private void initOrderSaga(){
+    System.out.println("Initializing order saga");
+    stateMachine = stateMachineFactory.getStateMachine();
+    stateMachine.startReactively().subscribe();
+    System.out.println("Final state: " + stateMachine.getState().getId());
+  }
+
+  private void stopOrderSaga() {
+    System.out.println("Stopping saga");
+    stateMachine.stopReactively().subscribe();  
+  }
+
+
+}
